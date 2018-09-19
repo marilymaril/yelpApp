812342d1a2cf902837c9e4544ec921aeb09192e6
marilymaril <mflorek@csumb.edu>
2018-09-19 08:27:55 -0700

    Initial Commit

---
+++ yelpApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yelpApp
+//
+//  Created by Marilyn Florek on 9/19/18.
+//  Copyright © 2018 Marilyn Florek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
